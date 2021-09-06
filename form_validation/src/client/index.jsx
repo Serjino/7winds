9d674423912ef71eb1b8bdf678096dc69aa748ad
serjino <serjino777@yandex.ru>
2021-09-06 16:14:18 +0300

    clean project

--- form_validation/src/client/index.jsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import * as ReactDom from 'react-dom';
-import { AppComponent } from '../shared/App.tsx';
-
-window.addEventListener('load', () => {
-    ReactDom.hydrate(<AppComponent />, document.getElementById('reactRoot'));
-    // module.hot.accept();
-});
--- load_file_component/src/client/index.jsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import * as ReactDom from 'react-dom';
-import { App } from '../shared/App';
-
-window.addEventListener('load', () => {
-    ReactDom.hydrate(<App />, document.getElementById('reactRoot'));
-    // module.hot.accept();
-});
