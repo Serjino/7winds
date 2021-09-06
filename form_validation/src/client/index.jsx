72fe2a3276c4e5be8c0ba89acaa9feac9e6a7791
serjino <serjino777@yandex.ru>
2021-09-06 14:32:19 +0300

    changed or not

--- form_validation/src/client/index.jsx
+++ form_validation/src/client/index.jsx
@@ -3,6 +3,6 @@ import * as ReactDom from 'react-dom';
 import { AppComponent } from '../shared/App.tsx';
 
 window.addEventListener('load', () => {
-    ReactDom.render(<AppComponent />, document.getElementById('reactRoot'));
+    ReactDom.hydrate(<AppComponent />, document.getElementById('reactRoot'));
     // module.hot.accept();
 });
