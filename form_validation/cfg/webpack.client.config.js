33c4de541b4be79a1fd082c2dc9cfe79c29f4042
serjino <serjino777@yandex.ru>
2021-09-06 13:35:56 +0300

    changedOrNot

--- form_validation/cfg/webpack.client.config.js
+++ form_validation/cfg/webpack.client.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
-const { HotModuleReplacementPlugin } = require('webpack');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const { HotModuleReplacementPlugin } = require('webpack');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const NODE_ENV = process.env.NODE_ENV;
 const IS_DEV = NODE_ENV === 'development';
@@ -19,19 +19,19 @@ function setupDevtool() {
 module.exports = {
     resolve: {
         extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-        alias: {
-            'react-dom': IS_DEV ? '@hot-loader/react-dom' : 'react-dom',
-        }
+        // alias: {
+        //     'react-dom': IS_DEV ? '@hot-loader/react-dom' : 'react-dom',
+        // }
     },
     mode: NODE_ENV ? NODE_ENV : 'development',
     entry: [
         path.resolve(__dirname, '../src/client/index.jsx'),
-        'webpack-hot-middleware/client?path=http://localhost:3001/static/__webpack_hmr',
+        // 'webpack-hot-middleware/client?path=http://localhost:3001/static/__webpack_hmr',
     ],
     output: {
-        path: path.resolve(__dirname, '../dist/client'),
-        filename: 'client.js',
-        publicPath: '/static/'
+        path: path.resolve(__dirname, '../../docs'),
+        filename: 'index.js',
+        // publicPath: '/static/'
     },
     module: {
         rules: [
@@ -56,7 +56,7 @@ module.exports = {
             },
             {
                 test: GLOBAL_CSS_REGEXP,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
+                use: ['style-loader', 'css-loader'],
             },
             {
                 test: /\.(png|jpg|gif|svg)$/i,
@@ -73,9 +73,9 @@ module.exports = {
             },
         ],
     },
-    devtool: setupDevtool(),
-    plugins: IS_DEV ? [
-        new CleanWebpackPlugin(),
-        new HotModuleReplacementPlugin(),
-    ] : [],
+    // devtool: setupDevtool(),
+    // plugins: IS_DEV ? [
+    //     new CleanWebpackPlugin(),
+    //     new HotModuleReplacementPlugin(),
+    // ] : [],
 };
